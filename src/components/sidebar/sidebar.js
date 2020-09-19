e93ac643e1b7a466daacfe0f66ab8da3e26934cc
venaz <chibuzo.madu@gmail.com>
2020-09-19 22:14:40 +0100

    added onclick event for menu items

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./sidebar.scss";
 
 import dashboard from "../../assets/icons/dashboard.svg";
@@ -6,60 +6,45 @@ import calender from "../../assets/icons/Calendar.svg";
 import list from "../../assets/icons/list.svg";
 import message from "../../assets/icons/Message.svg";
 import statistic from "../../assets/icons/Statistics.svg";
-import add from '../../assets/icons/Add.svg'
+import add from "../../assets/icons/Add.svg";
 
-const Sidebar = ({children}) => {
+const menuItems = [
+  { link: "Dashboard", icon: require("../../assets/icons/dashboard.svg") },
+  { link: "Courses", icon: require("../../assets/icons/dashboard.svg") },
+  { link: "Resources", icon: require("../../assets/icons/dashboard.svg") },
+  { link: "Tasks", icon: require("../../assets/icons/list.svg") },
+  { link: "Messages", icon: require("../../assets/icons/Message.svg") },
+  { link: "Schedule", icon: require("../../assets/icons/Calendar.svg") },
+  { link: "Reports", icon: require("../../assets/icons/Statistics.svg") },
+];
+
+const Sidebar = ({ children }) => {
+  const [active, SetActive] = useState("Resources");
+  const handleClick = (value) => {
+    SetActive(value);
+  };
   return (
     <div className="sidebar-container">
       <div className="sidebar">
-        <a href="#">
-          <div className="side-menu-item">
-            <img src={dashboard} />
-            <p className="menu-item-text"> Dashboard</p>
+        {menuItems.map((menu, index) => (
+          <div
+            key={index + 1}
+            className={`side-menu-item ${
+              active === menu.link ? "active" : null
+            }`}
+            onClick={() => handleClick(menu.link)}
+          >
+            <img src={menu.icon} />
+            <p className="menu-item-text"> {menu.link}</p>
           </div>
-        </a>
+        ))}
 
-        <a href="#">
-          <div className="side-menu-item">
-            <img src={dashboard} />
-            <p className="menu-item-text"> Courses</p>
-          </div>
-        </a>
-        <a href="#">
-          <div className="side-menu-item">
-            <img src={dashboard} />
-            <p className="menu-item-text"> Resources</p>
-          </div>
-        </a>
-        <a href="#">
-          <div className="side-menu-item">
-            <img src={list} />
-            <p className="menu-item-text"> Tasks</p>
-          </div>
-        </a>
-        <a href="#">
-          <div className="side-menu-item">
-            <img src={message} />
-            <p className="menu-item-text"> Messages</p>
-          </div>
-        </a>
-
-        <a href="#">
-          <div className="side-menu-item">
-            <img src={calender} />
-            <p className="menu-item-text"> Schedule</p>
-          </div>
-        </a>
-        <a href="#">
-          <div className="side-menu-item">
-            <img src={statistic} />
-            <p className="menu-item-text"> Reports</p>
-          </div>
-        </a>
         <div className="connect-apps pl-2 pr-2">
           <div className="title">
             <p className="text-white">Connect Apps</p>
-            <img src={add} />
+            <button className="bg-clear">
+              <img src={add} />
+            </button>
           </div>
           <div className="connect-item">
             <img src={statistic} />
@@ -77,15 +62,14 @@ const Sidebar = ({children}) => {
             <img src={statistic} />
             <p>Excel</p>
           </div>
-  
         </div>
         <div className="logout pl-2">
-            <button className="bg-secondary text-white">Logout  <img src={statistic} /></button>
+          <button className="bg-secondary text-white">
+            Logout <img src={statistic} />
+          </button>
         </div>
       </div>
-      <div className="content">
-        {children}
-      </div>
+      <div className="content">{children}</div>
     </div>
   );
 };
